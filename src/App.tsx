3b5bf8c8ded163d0603f76327484678f524b99e6
Na <lette1394+github@gmail.com>
2018-11-28 20:40:44 +0900

    ADD websocket lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
+import io from "socket.io";
 
-export default class App extends React.Component {
+interface Props {}
+interface State {}
+export default class App extends React.Component<Props, State> {
+  componentDidMount() {
+    const socket = io();
+
+    const interval = setInterval(() => socket.emit("message", "hello "), 1000);
+
+    setTimeout(() => clearInterval(interval), 5000);
+  }
+
   render() {
-    return <div>hello world!</div>;
+    return <div>sending message...</div>;
   }
 }
