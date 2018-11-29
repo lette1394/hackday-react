d29033e2b840511e51735895d063f62c0b1ea8c6
Na <lette1394+github@gmail.com>
2018-11-29 20:50:38 +0900

    FIX flex CSS layout

--- src/Status.tsx
+++ src/Status.tsx
@@ -8,8 +8,10 @@ const Status = ({ user, className }) => {
       <Title>현재 연결된 계정 정보</Title>
 
       {Object.keys(user).map((key) => (
-        <div>
-          {key} : {user[key]}
+        <div key={key}>
+          <span className="item">
+            {key} : {user[key]}
+          </span>
         </div>
       ))}
     </div>
@@ -22,6 +24,13 @@ const styledStatus = styled(Status)`
   min-width: 400px;
   border: 1px solid #e8e8e8;
   border-radius: 20px;
+
+  padding: 30px;
+  /* margin: 20px; */
+
+  .item {
+    font-size: 20px;
+  }
 `;
 
 export { styledStatus as Status };
